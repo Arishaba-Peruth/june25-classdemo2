e2191e2246e8394768b1ba801a37185ed5114d32
Arishaba-Peruth <lorisperutha@gmail.com>
2025-08-03 21:08:48 +0300

    corrected errors in my files

--- greeting/tests.py
+++ greeting/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase, Client
-from django.urls import reverse 
+from django.urls import reverse
 
 
 class GreetingTests(TestCase):
@@ -10,8 +10,8 @@ class GreetingTests(TestCase):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Hello')
-        self.assertTemplateUsed(response, 'index.html')
-        
+        self.assertTemplateUsed(response, 'index.html')        
+
 class GreetingFunctionalityTests(TestCase):
     def test_greeting(self):
         response = self.client.get(reverse('index'))
