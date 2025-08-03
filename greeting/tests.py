14e841272ad62fe4a9854ed8a1dbce71eb626272
Arishaba-Peruth <lorisperutha@gmail.com>
2025-08-03 21:17:03 +0300

    Fixed some errors in my tests

--- greeting/tests.py
+++ greeting/tests.py
@@ -5,12 +5,12 @@ from django.urls import reverse
 class GreetingTests(TestCase):
     def setUp(self):
         self.client = Client()
-   
+
     def test_index_page(self):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Hello')
-        self.assertTemplateUsed(response, 'index.html')   
+        self.assertTemplateUsed(response, 'index.html')
 
 
 class GreetingFunctionalityTests(TestCase):
